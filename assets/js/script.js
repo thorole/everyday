6137912bbab7527112fb2629417eeed9456b386c
Thor-Ole Øby <punkthor@gmail.com>
2020-11-12 15:18:01 +0100

    Added options wrapper.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,36 +7,45 @@ let wrappers = document.getElementsByClassName("wrapper");
 for (let i = 0, j = 0; i < selects.length; i++, j++) {
     selects[i].style.display = "none";                          // Hide original select
     let options = selects[i].children;                          // Get the options  
-    let defaultOption = document.createElement("div");          // Create replacement for select
+    let defaultOption = document.createElement("div");          // Create default option element
+    let optionsWrapper = document.createElement("div");         // Create options wrapper
     defaultOption.innerHTML = options[0].innerHTML;             // Set default 
-    defaultOption.classList.add("all-options");             
-    wrappers[j].appendChild(defaultOption);                     // Append select to wrapper
-
+    defaultOption.classList.add("all-options");
+    wrappers[j].appendChild(defaultOption);                     // Append the default option element
+    wrappers[j].appendChild(optionsWrapper);                    // Append options wrapper 
+    defaultOption.addEventListener("click", function () {       // Eventlistener for open/close options wrapper
+        openClose(optionsWrapper);
+    });                                                          
     for (let i = 0; i < options.length; i++) {                  // Loop through options and create the custom select
         let opt = document.createElement("div");                // Create replacement for option
-        opt.classList.add("option")                         
-        opt.style.display = "none";                             // Hide options
+        opt.classList.add("option");
+        optionsWrapper.style.display = "none";                  // Hide options
         opt.innerHTML = options[i].innerHTML;                   // Set option text according to original option
-        wrappers[j].appendChild(opt);                           // Append option to wrapper
+        optionsWrapper.appendChild(opt);                        // Append option to wrapper
         opt.addEventListener("click", function () {             // Wait for click on any option
             defaultOption.innerHTML = this.innerHTML;           // Set default option text equal to option clicked
             for (let k = 0; k < options.length; k++) {          // Loop through original options
                 if (options[i].innerHTML === this.innerHTML) {  // If same option
-                    options[i].selected = true;                 // Select the same option in original select  
+                    options[i].selected = true;                 // Select the same option in original select
                 }
-            }     
+            }
         });
-        wrappers[j].addEventListener("click", function () {
-            
-            if (opt.style.display == "block") {                 // If visible, hide
-                opt.style.display = "none";
-            }
-            else {                                              // else display
-                opt.style.display = "block";
-            }
+        opt.addEventListener("click", function () {
+            openClose(optionsWrapper);
         });
     }
 }
+
+function openClose(elem) {
+    if (elem.style.display == "block") {                 // If visible, hide
+        elem.style.display = "none";
+    }
+    else {                                              // else display
+        elem.style.display = "block";
+    }
+}
+
+
 
 
 
